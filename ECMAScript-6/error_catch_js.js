99b2f7a006fdf991615abeb40d918beda694d966
Ajmain Nishu <ajmainnishu@gmail.com>
2021-07-26 16:50:36 +0600

    Delete error all files

--- ECMAScript-6/error_catch_js.js
+++
@@ -1,9 +0,0 @@
-//catch binding
-try {
-    console.log('I am Line 1')
-    throw new Error('I am Error')
-    console.log('I am Line 2')
-    console.log('I am Line 3')
-} catch {
-    console.log('This is custom error message')
-}
