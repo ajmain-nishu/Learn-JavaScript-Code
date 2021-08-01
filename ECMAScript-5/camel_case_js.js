932f6f508c1ef94946a9fb3b7078c2496a770a3e
Ajmain Nishu <ajmainnishu@gmail.com>
2021-08-02 02:00:08 +0600

    test: Rename file name and comment

--- ECMAScript-5/camel_case_js.js
+++
@@ -1,6 +0,0 @@
-//variable structure format
-var mathNumber = 10
-
-var mathNumberDetailsId = 104
-
-var math_number = 4
---
+++ ECMAScript-5/case_js.js
@@ -0,0 +1,8 @@
+//most common programming case type
+var camelCase = 10
+
+var kebab-case = 20
+
+var snake_case = 30
+
+var PascalCase = 40
